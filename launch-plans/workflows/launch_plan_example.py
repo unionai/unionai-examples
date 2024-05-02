b36a071d66080280412d9486e6cc7ff58dd0a478
Peeter Piegaze <1153481+ppiegaze@users.noreply.github.com>
2024-05-02 13:11:53 +0200

    update

--- launch-plans/workflows/launch_plan_example.py
+++ launch-plans/workflows/launch_plan_example.py
@@ -1,18 +1,10 @@
-from flytekit import task, workflow, LaunchPlan
-
-@task
-def my_task(a: int, b: int, c: int) -> int:
-    return a + b + c
-
-@workflow
-def my_workflow(a: int, b: int, c: int) -> int:
-    return my_task(a=a, b=b, c=c)
+from flytekit import LaunchPlan, FixedRate
+from datetime import timedelta
+from .workflow_example import my_workflow
 
 LaunchPlan.get_or_create(
     workflow=my_workflow,
     name="my_workflow_custom_lp",
-    fixed_inputs={"a": 3},
-    default_inputs={"b": 4, "c": 5},
-    schedule="*/1 * * * *"
+    fixed_inputs={"a": 6},
+    default_inputs={"b": 4, "c": 5}
 )
-
