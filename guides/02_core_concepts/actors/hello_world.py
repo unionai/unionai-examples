48776c4d17f708d747febe9468554e229ad3c11b
nikki everett <nikki@union.ai>
2024-06-10 15:19:32 -0500

    add actors examples
    Signed-off-by: nikki everett <nikki@union.ai>

---
+++ guides/02_core_concepts/actors/hello_world.py
@@ -0,0 +1,24 @@
+from flytekit import workflow, Resources
+from unionai.actor import ActorEnvironment
+
+actor = ActorEnvironment(
+    replica_count=1,
+    parallelism=1,
+    backlog_length=10,
+    ttl_seconds=30,
+    requests=Resources(
+        cpu="2",
+        mem="300Mi",
+        ),
+    container_image="ghcr.io/fiedlernr9/unionai-actors:0.3",
+)
+
+
+@actor
+def say_hello() -> str:
+    return "hello"
+
+
+@workflow
+def wf():
+    say_hello()
