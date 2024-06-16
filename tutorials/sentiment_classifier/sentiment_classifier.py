919dbfdbc68357ade4f2191ead42a06f86014282
Niels Bantilan <niels.bantilan@gmail.com>
2024-06-16 08:28:20 -0400

    update sentiment classifier example with markdown and yaml frontmatter
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- tutorials/sentiment_classifier/sentiment_classifier.py
+++ tutorials/sentiment_classifier/sentiment_classifier.py
@@ -1,21 +1,29 @@
-"""Runs sentiment classification workflow with wandb integration.
-
-1. Download dataset
-2. Download model weights
-3. Train model and upload data to wandb
-4. Configure artifact metadata in wandb
-5. Run a second workflow with another model and compare models
-
-Create a secret for wandb :
-```
-unionai create secret wandb_api_key
-```
-
-Usage:
-```
-unionai run --remote tutorials/sentiment_classifier/sentiment_classifier.py main
-```
 """
+---
+run_commands:
+  - git clone https://github.com/unionai/examples
+  - cd examples
+  - unionai run --remote tutorials/sentiment_classifier/sentiment_classifier.py main --model distilbert-base-uncased
+---
+"""
+
+# # Sentiment Classification with Language Models
+#
+# This tutorial demonstrates how to fine-tune a pre-trained language model to
+# classify the sentiment of IMDB movie reviews. We're going to use the
+# `transformers` library and the `imdb` dataset to classify the sentiment of movie
+# review.
+
+# ## Overview
+#
+# The power of language models lies in their flexibility: as long as you
+# operate in the same token space as a pre-trained model, you can leverage the
+# patterns learned from a much wider data distribution than you could learn from
+# just a small data domain.
+#
+# In this example, we're going to fine-tune the [DistilBERT](https://huggingface.co/distilbert/distilbert-base-uncased) model on the [IMDB dataset](https://huggingface.co/datasets/stanfordnlp/imdb) to classify the sentiment of movie reviews.
+#
+# We'll start by importing the workflow dependencies:
 
 from pathlib import Path
 import tarfile
@@ -25,6 +33,10 @@ from flytekit.extras import accelerators
 from flytekit.types.directory import FlyteDirectory
 from flytekit.types.file import FlyteFile
 
+# ## Defining the container image
+#
+# We'll define the container image that will be used to run the workflow with
+# the `ImageSpec` object:
 
 image_spec = ImageSpec(
     packages=[
@@ -38,6 +50,15 @@ image_spec = ImageSpec(
     cuda="11.8",
 )
 
+# We've pinned the versions of the package dependencies to ensure reproducibility.
+# Under the hood, Union will build the container image so we don't have to
+# worry about writing a `Dockerfile`.
+#
+# ## Downloading the dataset and model
+#
+# Next, we download the dataset. Specifying `cache=True` in the `@task`
+# definition makes sure that we don't waste compute resources downloading the
+# data multiple times:
 
 @task(
     container_image=image_spec,
@@ -55,6 +76,7 @@ def download_dataset() -> FlyteDirectory:
 
     return dataset_cache_dir
 
+# Then we'll do the same for the model:
 
 @task(
     container_image=image_spec,
@@ -73,6 +95,16 @@ def download_model(model: str) -> FlyteDirectory:
     AutoModelForSequenceClassification.from_pretrained(model, cache_dir=model_cache_dir)
     return model_cache_dir
 
+# ## Fine-tuning the model
+#
+# Now we're ready to fine-tune the model using the dataset and model from the previous
+# steps. The task below does the following:
+#
+# 1. Loads the dataset and model.
+# 2. Tokenizes the dataset.
+# 3. Initializes a weights and biases session to track the training process.
+# 4. Trains the model based on the number of epochs (`n_epochs`) specified.
+# 5. Compresses the model to a tarfile and saves it to the specified path.
 
 @task(
     container_image=image_spec,
@@ -175,6 +207,9 @@ def train_model(
 
     return wandb_url, inference_path_compressed
 
+# ## Creating the workflow
+#
+# We can put all of these tasks together into a workflow:
 
 @workflow
 def main(
@@ -192,4 +227,14 @@ def main(
         model_cache_dir=model_cache_dir,
         dataset_cache_dir=dataset_cache_dir,
     )
- 
+
+# Each task is actually running in its own container, but Union takes care of
+# storing the intermediate outputs and passing them between tasks.
+#
+# ## Trying out different models
+#
+# Now that you've run the fine-tuning workflow once, you can try out different
+# models by passing in a different model name to the `model` argument, which can
+# be supplied to the `--model` flag when you invoke `unionai run`. For example,
+# you can try out the `google-bert/bert-base-uncased` model, or any text
+# [classification model](https://huggingface.co/models?pipeline_tag=text-classification&sort=trending) available on HuggingFace hub.
