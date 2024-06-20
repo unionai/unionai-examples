141b73cc6ed224aa6c1a46009f5fb2756675d409
Daniel Sola <daniel.sola@union.ai>
2024-06-20 13:41:24 -0700

    fix nits
    Signed-off-by: Daniel Sola <daniel.sola@union.ai>

--- tutorials/reddit_slack_bot/reddit_slack_bot.py
+++ tutorials/reddit_slack_bot/reddit_slack_bot.py
@@ -6,28 +6,32 @@
 # workflows at constant interval, so we never miss a Reddit post we might be
 # interested in.
 
-# ## Overview
+# ## Creating Secrets to Access Reddit and Slack
 #
 # For our workflow to access Reddit and Slack, we need to set up some credentials
 # with Slack and Reddit, and share them with our workflow in a safe way. For Reddit,
-# we can create a [developer app](https://www.reddit.com/prefs/apps) which will give
+# we create a [developer app](https://www.reddit.com/prefs/apps) which will give
 # us a client ID and secret. We will refer to these as `reddit_client_id` and `reddit_secret_key`.
-# For Slack, we will need a Slack API bot token which can be configured and created for
-# a specific channel by following
-# [Slack's documentation](https://api.slack.com/tutorials/tracks/getting-a-token). We
-# will call this token `reddit_slack_token`.
-#
-# Once we have collected our `reddit_client_id`, `reddit_secret_key`, and `reddit_slack_token`,
-# we can securely store them using the `unionai` CLI tool. To add `reddit_client_id` we can run:
+# Once we have collected our `reddit_client_id` and `reddit_secret_key`,
+# we securely store them using the `unionai` CLI tool. To add `reddit_client_id` we run:
 # ```bash
 # unionai create secret reddit_client_id
 # ```
 # and paste the client ID when prompted.
 #
-# The same can then be done for `reddit_secret_key`, and `reddit_slack_token`.
-#
-# Once there are set up, can can continue by importing some of the workflow dependencies
-# and setting some constatnts like the frequency in which we want to run the workflow and
+# After, we can do the same for `reddit_secret_key`.
+
+# For Slack, we will need a Slack API bot token which can be configured and created for
+# a specific channel by following
+# [Slack's documentation](https://api.slack.com/tutorials/tracks/getting-a-token). We
+# will call this token `slack_token`.  Similar to how we added `reddit_client_id` and `reddit_secret_key`,
+# we securely store the `slack_token` in the CLI using:,
+# ```bash
+# unionai create secret slack_token
+# ```
+
+# Once our secrets are set up, we continue by importing some of the workflow dependencies
+# and setting some constants like the frequency in which we want to run the workflow and
 # the Slack channel we want to post to:
 
 from datetime import datetime, timedelta
@@ -37,29 +41,27 @@ from flytekit import task, workflow, ImageSpec, Secret, current_context, LaunchP
 import requests
 from requests.auth import HTTPBasicAuth
 
-WEEKS_BETWEEN_RUNS = 1
+DAYS_BETWEEN_RUNS = 1
 SLACK_CHANNEL_NAME = '#reddit-posts'
 
 # ## Defining a Container Image
 #
 # The previously imported packages are either in the Python Standard Library or included by
 # default in the base flyte image used by Union. However, if we want to include additional
-# packages we can define an `ImageSpec` which will create an image under the hood, so we don't
+# packages we define an `ImageSpec` which will create an image under the hood, so we don't
 # have to worry about writing a `Dockerfile`.
 
 image = ImageSpec(
     builder="unionai",
-    packages=[
-            "slack_sdk==3.28.0",
-        ]
+    packages=["slack_sdk==3.28.0"]
 )
 
 # ## Collecting Reddit Posts
 #
-# To allow for our task to use our `reddit_client_id` and `reddit_secret_key`, we simply
+# For our task, to use `reddit_client_id` and `reddit_secret_key`, we
 # specify them in the `@task` decorator using the `secret_requests` argument. Then all we need
 # to do is format a request and parst the result. We will only return posts since the last
-# time we ran the workflow; specified by `WEEKS_BETWEEN_RUNS`.
+# time we ran the workflow; specified by `DAYS_BETWEEN_RUNS`.
 
 @task(
     secret_requests=[
@@ -80,19 +82,19 @@ def get_posts(search_terms: List[str]) -> List[Dict[str, str]]:
     posts = result_json['data']['children']
 
     # Get only recent posts
-    weeks_ago_datetime = datetime.utcnow() - timedelta(weeks=WEEKS_BETWEEN_RUNS)
-    weeks_ago_timestamp = weeks_ago_datetime.timestamp()
+    days_ago_datetime = datetime.utcnow() - timedelta(days=DAYS_BETWEEN_RUNS)
+    days_ago_timestamp = days_ago_datetime.timestamp()
     recent_posts = [{
         'title': post['data']['title'],
         'description': post['data']['selftext'],
         'link': post['data']['url']
-    } for post in posts if post['data']['created_utc'] >= weeks_ago_timestamp]
+    } for post in posts if post['data']['created_utc'] >= days_ago_timestamp]
 
     return recent_posts
 
 # ## Posting to Slack
 #
-# Given the reddit posts returned by our previous task, we can define a helper function to format them
+# Given the reddit posts returned by our previous task, we define a helper function to format them
 # in a more readable format for Slack.
 
 def format_posts(posts: List[Dict[str, str]]):
@@ -102,18 +104,18 @@ def format_posts(posts: List[Dict[str, str]]):
         formatted_message += f"{post['description']}\n\n"
     return formatted_message
 
-# We can then define another task to send our posts to slack. This time we can make use of our `ImageSpec`
-# we previously defined that contains our `slack_sdk` package.
+# We define another task to send our posts to slack. This time we use the `ImageSpec`
+# we previously wrote that contains the `slack_sdk` package.
 
 @task(
     container_image=image,
-    secret_requests=[Secret(key="reddit_slack_token")]
+    secret_requests=[Secret(key="slack_token")]
 )
 def post_slack_message(recent_posts: List[Dict[str, str]]):
     from slack_sdk import WebClient
 
-    reddit_slack_token = current_context().secrets.get("reddit_slack_token")
-    client = WebClient(token=reddit_slack_token)
+    slack_token = current_context().secrets.get("slack_token")
+    client = WebClient(token=slack_token)
 
     response = client.chat_postMessage(
         channel=SLACK_CHANNEL_NAME,
@@ -123,7 +125,7 @@ def post_slack_message(recent_posts: List[Dict[str, str]]):
 
 # ## Creating the Workflow
 #
-# Finally, we can chain these tasks together int a simple two-step workflow with some default inputs.
+# Finally, we chain these tasks together into a simple two-step workflow with some default inputs.
 
 @workflow
 def reddit_wf(search_terms: List[str]= ["flyte", "ml"]):
@@ -132,31 +134,28 @@ def reddit_wf(search_terms: List[str]= ["flyte", "ml"]):
 
 # ## Defining a Schedule
 #
-# Using our previously created workflow, we can define multiple `LaunchPlans` that will
-# run on a schedule defined by standard `cron` expression. In this example we have one
-# `LaunchPlan` querying for posts containing "flyte" and "ml", and another `LaunchPlan`
-# querying for posts containing "union.ai".
+# Using our previously created workflow, we define a `LaunchPlan` that will
+# run on a schedule defined by a standard `cron` expression. In this example we have a
+# `LaunchPlan` querying for posts containing "flyte" and "ml".
 
 LaunchPlan.get_or_create(
     reddit_wf,
     name="flyte_reddit_posts",
     default_inputs={"search_terms": ["flyte", "ml"]},
     schedule=CronSchedule(
-        schedule=f"0 0 */{WEEKS_BETWEEN_RUNS*7} * *",
+        schedule=f"0 0 */{DAYS_BETWEEN_RUNS} * *",
     )
 )
 
-LaunchPlan.get_or_create(
-    reddit_wf,
-    name="union_reddit_posts",
-    default_inputs={"search_terms": ["union.ai"]},
-    schedule=CronSchedule(
-        schedule=f"0 0 */{WEEKS_BETWEEN_RUNS*7} * *",
-    )
-)
+# TO register and activate this `LaunchPLan` we can run:
+# ```bash
+# unionai register <path/to/reddit_slack_bot/>
+# unionai launchplan flyte_reddit_posts --activate
+# ```
 
-# After the `LaunchPlan` has been (registered)[https://docs.union.ai/serverless/development-cycle/registering-workflows#remote-cluster-using-fast-registration]
-# using `unionai register <path/to/redit_slack_bot/>`, it must be [activated](https://docs.union.ai/byoc/core-concepts/launch-plans/activating-and-deactivating)
-# either using the UI, CLI, or `UnionRemote`.
+# Our workflow will now run on the configured schedule until we deactivate the `LaunchPlan` either in the UI or using:
+# ```bash
+# unionai launchplan flyte_reddit_posts --deactivate
+# ```
 
 
