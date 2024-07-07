76245fc5a4c9c362052f47eb5fc8a4816d963adc
Ketan Umare <kumare3@users.noreply.github.com>
2024-07-06 23:40:00 -0700

    Embedding workflows
    1. Embed any pdf on cpu using actors and in parallel
    2. Embed all of wikipedia
    
    Goal:
    We need support for dynamic tasks on actors - this needs us to make a modification to actor.
    Show how models loaded in memory can make things go fast.

---
+++ tutorials/embed_wikipedia/embed.py
@@ -0,0 +1,107 @@
+import functools
+import logging
+import pathlib
+import typing
+
+import pandas as pd
+import torch
+from datasets import load_dataset_builder, DownloadConfig
+from flytekit import ImageSpec, task, Resources, workflow, dynamic, StructuredDataset
+from flytekit.core.utils import timeit
+from sentence_transformers import SentenceTransformer
+
+embedding_image = ImageSpec(
+    requirements="requirements.txt",
+    registry="ghcr.io/unionai-oss",
+    python_version="3.11",
+)
+
+ACTOR_MODE = True
+
+if ACTOR_MODE:
+    from union.actor import ActorEnvironment
+
+    actor = ActorEnvironment(
+        name="embedding-actor",
+        replica_count=2 * 8,
+        parallelism=1,
+        backlog_length=0,
+        ttl_seconds=300,
+        requests=Resources(gpu="1", mem="12Gi", cpu="7"),
+        # accelerator=A10G,
+        container_image=embedding_image,
+        # secrets=secret_requests=[Secret(key="hf_token")],
+    )
+else:
+    actor = functools.partial(task, container_image=embedding_image, requests=Resources(mem="12Gi", cpu="8", gpu="1"))
+    # ,)secret_requests=[Secret(key="hf_token")])
+
+encoder: typing.Optional[SentenceTransformer] = None
+
+
+@timeit("load_model")
+def load_model(model_name: str = 'msmarco-MiniLM-L-6-v3') -> SentenceTransformer:
+    global encoder
+    if encoder:
+        return encoder
+    encoder = SentenceTransformer(model_name)
+    encoder.max_seq_length = 256
+    return encoder
+
+
+@actor(cache=True, cache_version="1.8")
+def encode(model: str, df: pd.DataFrame, batch_size: int) -> torch.Tensor:
+    bi_encoder = load_model(model)
+    print(f"Loaded encoder into device: {bi_encoder.device}")
+    with timeit("encode"):
+        return bi_encoder.encode(df["text"], convert_to_tensor=True, show_progress_bar=True, batch_size=batch_size)
+
+
+@task(container_image=embedding_image, requests=Resources(mem="4Gi", cpu="3"), retries=0, cache=True,
+      cache_version="1.0")
+def list_partitions(name: str, version: str, num_proc: int) -> typing.List[StructuredDataset]:
+    dsb = load_dataset_builder(name, version, cache_dir="/tmp/hfds")
+    logging.log(logging.INFO, f"Downloading {name} {version}")
+    with timeit("download"):
+        dsb.download_and_prepare(file_format="parquet",
+                                 download_config=DownloadConfig(disable_tqdm=True, num_proc=num_proc))
+    logging.log(logging.INFO, f"Download complete")
+    p = pathlib.Path(dsb.cache_dir)
+    i = 0
+    partitions = []
+    for f in p.iterdir():
+        if "parquet" in f.name:
+            print(f"Encoding {i}: {f}")
+            partitions.append(StructuredDataset(uri=str(f)))
+        i += 1
+    return partitions
+
+
+@dynamic(container_image=embedding_image, cache=True, cache_version="2.3")
+def dynamic_encoder(partitions: typing.List[StructuredDataset], embedding_model: str, batch_size: int) -> typing.List[
+    torch.Tensor]:
+    embeddings = []
+    for p in partitions:
+        embeddings.append(encode(model=embedding_model, df=p, batch_size=batch_size))  # noqa
+    return embeddings
+
+
+@workflow
+def embed_wikipedia(
+        name: str = "wikipedia",
+        version: str = "20220301.simple",
+        num_proc: int = 4,
+        embedding_model: str = "BAAI/bge-small-en-v1.5",
+        batch_size: int = 128,
+) -> typing.List[torch.Tensor]:
+    partitions = list_partitions(name=name, version=version, num_proc=num_proc)
+    return dynamic_encoder(partitions=partitions, embedding_model=embedding_model, batch_size=batch_size)  # noqa
+
+
+if __name__ == '__main__':
+    df = pd.read_parquet("/Users/ketanumare/src/data/wiki-2.parquet")
+    s = df["text"][:1000]
+    new_df = pd.DataFrame(s)
+    m = 'msmarco-MiniLM-L-6-v3'
+    m = "BAAI/bge-small-en-v1.5"
+    encode(model=m, df=new_df, batch_size=64)
---
+++ tutorials/pdf_embedding/pdf_embedder.py
@@ -0,0 +1,124 @@
+import typing
+from typing import List
+
+import numpy as np
+from flytekit import ImageSpec, dynamic, Resources, Secret, workflow
+from flytekit.core.utils import timeit
+from flytekit.types.file import FlyteFile
+from sentence_transformers import SentenceTransformer
+import fitz
+import spacy
+from union.actor import ActorEnvironment
+
+embedding_image = ImageSpec(
+    requirements="requirements.txt",
+    python_version="3.11",
+    registry="ghcr.io/unionai-oss",
+)
+
+cpu_actor = ActorEnvironment(
+    name="embedding-actor",
+    replica_count=8,
+    parallelism=1,
+    backlog_length=2,
+    ttl_seconds=300,
+    requests=Resources(cpu="2", mem="8Gi"),
+    container_image=embedding_image,
+)
+
+# gpu_actor = ActorEnvironment(
+#     name="embedding-actor",
+#     replica_count=1,
+#     parallelism=1,
+#     backlog_length=5,
+#     ttl_seconds=300,
+#     requests=Resources(gpu="1", mem="8Gi", cpu="4"),
+#     accelerator=A10G,
+#     container_image=embedding_image,
+#     # secrets=secret_requests=[Secret(key="hf_token")],
+# )
+
+DEFAULT_MODEL = "jinaai/jina-embeddings-v2-base-en"
+
+encoder = None
+nlp = None
+
+
+@timeit("load_nlp_model")
+def load_spacy_model() -> spacy.Language:
+    global nlp
+    if nlp:
+        return nlp
+    nlp = spacy.load("en_core_web_sm")
+    return nlp
+
+
+@cpu_actor(cache=True, cache_version="1.0")
+def pdf_to_text(pdf_uri: FlyteFile) -> typing.List[str]:
+    nlp = load_spacy_model()
+    with pdf_uri.open("rb") as f:
+        doc = fitz.open(stream=f)
+        all_sentences = []
+        for page_num in range(len(doc)):
+            # Load the page
+            page = doc.load_page(page_num)
+            text = page.get_text()
+            # Extract sentences
+            spacy_doc = nlp(text)
+            sentences = [sent.text for sent in spacy_doc.sents]
+            # Add the sentences to the list
+            all_sentences.extend(sentences)
+        return all_sentences
+
+
+@timeit("load_model")
+def load_model(model_name: str = 'msmarco-MiniLM-L-6-v3') -> SentenceTransformer:
+    global encoder
+    if encoder:
+        return encoder
+    encoder = SentenceTransformer(model_name)
+    encoder.max_seq_length = 256
+    return encoder
+
+
+@cpu_actor(cache=True, cache_version="1.0")
+def encode(embedding_model_name: str, text: List[str], batch_size: int = 64) -> List[List[float]]:
+    model = load_model(embedding_model_name)
+    embeddings: np.ndarray = model.encode(text, batch_size=batch_size)
+    return [e.round(6).tolist() for e in embeddings]
+
+
+@dynamic(container_image=embedding_image, requests=Resources(mem="8Gi"), cache=True, cache_version="1.0")
+def process_all_sentences(
+        embedding_model_name: str,
+        sentences: List[str],
+        chunk_size: int,
+        batch_size: int = 64,
+) -> List[List[List[float]]]:
+    results = []
+    for chunk, _ in enumerate(sentences[::chunk_size]):
+        s = sentences[chunk:chunk + chunk_size]
+        r = encode(embedding_model_name=embedding_model_name, text=s, batch_size=batch_size)
+        results.append(r)
+    return results
+
+
+@workflow
+def wf(
+        pdf: FlyteFile = "https://huggingface.co/datasets/amitsaurav/req-doc-samples/resolve/main/amazon-dynamo-sosp2007.pdf",
+        embedding_model: str = DEFAULT_MODEL,
+        chunk_size: int = 10,
+        batch_size: int = 64,
+) -> (typing.List[str], typing.List[typing.List[float]]):
+    sentences = pdf_to_text(pdf_uri=pdf)
+    results = process_all_sentences(
+        embedding_model_name=embedding_model, sentences=sentences, chunk_size=chunk_size, batch_size=batch_size)
+    return sentences, results
+
+
+@workflow
+def try_sentences(embedding_model: str = DEFAULT_MODEL, chunk_size: int = 10) -> typing.Tuple[
+    typing.List[str], List[List[List[float]]]]:
+    sentences = ["hello world", "this is a test", "this is a test", "this is a test", "this is a test"]
+    results = process_all_sentences(embedding_model_name=embedding_model, sentences=sentences, chunk_size=chunk_size)
+    return sentences, results
