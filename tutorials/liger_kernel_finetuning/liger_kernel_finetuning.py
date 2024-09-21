4b9313a56f2eb679f505da05b8c04c6560de7969
Niels Bantilan <niels.bantilan@gmail.com>
2024-09-20 20:50:52 -0400

    finalize workflow, add image
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- tutorials/liger_kernel_finetuning/liger_kernel_finetuning.py
+++ tutorials/liger_kernel_finetuning/liger_kernel_finetuning.py
@@ -1,9 +1,23 @@
 # # Benchmarking the Efficiency of LLM Finetuning with the Liger Kernel
 #
+# [Liger Kernel](https://github.com/linkedin/Liger-Kernel) is a set of Triton kernels
+# designed specifically for LLM training. It can increase GPU training throughput by
+# up to 20% and reduces memory usage by up to 60%.
+#
+# The official repo contains [benchmark results](https://github.com/linkedin/Liger-Kernel/tree/main/examples/huggingface)
+# for several larger models, like Llama3-8B, Qwen2-7B, and Gemma-7B, which shows the
+# performance benefits of using the components of the Liger Kernel.
+#
 # This tutorial demonstrates how to run a fine-tuning benchmarking experiment
-# using the Liger kernel. We'll use the HuggingFace `transformers` library and the
-# `alpaca` dataset to finetune a pre-trained Llama 3 8B model with and without the
-# Liger kernel and compare the results.
+# using the Liger kernel on a smaller language model, Phi3 mini, a 3.8B parameter
+# model. We'll use the HuggingFace `transformers` library and the `alpaca` dataset
+# and compare token throughput and peak memory usage between using the Liger kernel
+# vs not using it.
+#
+# We'll scale down the experiment to train on a sequence length of 128 tokens so that
+# each run trains on a single A100 GPU.
+#
+# First, let's import the necessary libraries:
 
 import itertools
 import json
@@ -34,28 +48,35 @@ from flytekitplugins.wandb import wandb_init
 import transformers
 from callback import EfficiencyCallback
 
+# ## Define Python dependencies
+#
+# Then we'll define the image specification for the container we'll use to run the
+# experiment.
 
 image = ImageSpec(
     name="liger-kernel-finetuning",
     packages=[
+        "datasets==2.21.0",
         "flytekitplugins-deck-standard",
         "flytekitplugins-wandb",
-        "datasets==2.21.0",
-        "pandas==2.2.2",
-        "matplotlib==3.9.2",
         "huggingface-hub==0.24.6",
+        "liger-kernel==0.2.1",
+        "matplotlib==3.9.2",
+        "pandas==2.2.2",
+        "seaborn==0.13.2",
         "transformers==4.43.3",
         "trl==0.10.1",
         "torch==2.4.1",
-        "liger-kernel==0.2.1",
     ],
     apt_packages=["build-essential"],
     cuda="12.1",
     registry=os.environ.get("IMAGE_SPEC_REGISTRY"),
 )
 
-WANDB_PROJECT = "liger-kernel-finetuning"
-WANDB_ENTITY = "niels-bantilan"
+# ## Define types and global variables
+
+WANDB_PROJECT = "liger-kernel-finetuning"  # replace this with your wandb project name
+WANDB_ENTITY = "union-examples"  # replace this with your wandb entity (username or team name)
 
 
 @dataclass
@@ -64,6 +85,7 @@ class CustomArguments:
     dataset_name: str = "tatsu-lab/alpaca"
     max_seq_length: int = 512
     use_liger: bool = False
+    run_number: Optional[int] = None
 
 
 @dataclass
@@ -100,6 +122,8 @@ class TrainingResult:
     training_history: FlyteFile
     training_args: TrainingArguments
 
+
+# ## Model training task
 
 WANDB_SECRET = Secret(key="wandb_api_key")
 
@@ -196,6 +220,9 @@ def train_model(training_args: TrainingArguments) -> TrainingResult:
     )
 
 
+# ## Preparing the grid search space for the experiment
+
+
 @task(
     container_image=image,
     cache=True,
@@ -204,27 +231,36 @@ def train_model(training_args: TrainingArguments) -> TrainingResult:
 def prepare_experiment_args(
     experiment_args: ExperimentArguments,
     training_args: TrainingArguments,
+    n_runs: int,
 ) -> list[TrainingArguments]:
     training_args_list = []
-    for use_liger, bs in itertools.product(
-        *[experiment_args.use_liger, experiment_args.per_device_train_batch_size],
+    for run_number, use_liger, bs in itertools.product(
+        *[
+            range(1, n_runs + 1),
+            experiment_args.use_liger,
+            experiment_args.per_device_train_batch_size,
+        ],
     ):
         args = deepcopy(training_args)
         args.use_liger = use_liger
         args.per_device_train_batch_size = bs
+        args.run_number = run_number
         training_args_list.append(args)
     return training_args_list
+
+
+# ## Analyzing the experiment results
 
 
 @task(
     container_image=image,
     enable_deck=True,
-    limits=Resources(mem="8Gi", cpu="4"),
+    limits=Resources(mem="2Gi", cpu="2"),
 )
 def analyze_results(results: list[Optional[TrainingResult]]) -> pd.DataFrame:
     import matplotlib.pyplot as plt
+    import seaborn as sns
 
-    ctx = current_context()
     history_columns = ["epoch", "step"]
     metrics = [
         "step_tokens_per_second",
@@ -243,62 +279,107 @@ def analyze_results(results: list[Optional[TrainingResult]]) -> pd.DataFrame:
         result.training_history.download()
         exp_vars = {k: getattr(result.training_args, k) for k in experiment_vars}
         with open(result.training_history.path, "r") as f:
-            dataframe.append(pd.read_json(f).assign(**exp_vars))
+            run_results = pd.read_json(f).assign(
+                run_number=result.training_args.run_number,
+                **exp_vars,
+            )
+            dataframe.append(run_results)
 
     dataframe = pd.concat(dataframe)
-    analysis_df = (
-        dataframe[history_columns + experiment_vars + metrics].dropna().drop_duplicates()
-    )
+    analysis_columns = ["run_number", *history_columns, *experiment_vars, *metrics]
+    analysis_df = dataframe[analysis_columns].dropna().drop_duplicates()
     # try converting experiment_vars to numeric types
     for col in experiment_vars:
         analysis_df[col] = pd.to_numeric(analysis_df[col], errors="ignore", downcast="integer")
 
-    grpby = analysis_df.groupby(experiment_vars)
-    avg_tokens_per_second = grpby.avg_tokens_per_second.last().to_frame()
+    grpby = analysis_df.groupby(experiment_vars + ["run_number"])
+    avg_tokens_per_second = (
+        grpby.step_tokens_per_second.mean().rename("avg_tokens_per_second").to_frame()
+    )
     step_peak_memory_reserved_mb = grpby.step_peak_memory_reserved_MB.max().to_frame()
 
-    benchmark_deck = Deck("Benchmarking Results")
-    fig, ax = plt.subplots(1, 2)
-    avg_tokens_per_second.plot.barh(ax=ax[0])
-    step_peak_memory_reserved_mb.plot.barh(ax=ax[1])
+    fig, ax = plt.subplots(2, 1, figsize=(12, 10))
+    sns.barplot(
+        x="per_device_train_batch_size",
+        y="avg_tokens_per_second",
+        hue="use_liger",
+        data=avg_tokens_per_second.reset_index(),
+        ax=ax[0],
+    )
+
+    sns.barplot(
+        x="per_device_train_batch_size",
+        y="step_peak_memory_reserved_MB",
+        hue="use_liger",
+        data=step_peak_memory_reserved_mb.reset_index(),
+        ax=ax[1],
+    )
+    sns.despine()
 
     # define deck reports
+    benchmark_deck = Deck("Benchmarking Results")
     benchmark_deck.append(_convert_fig_into_html(fig))
-    benchmark_deck.append(TableRenderer().to_html(df=avg_tokens_per_second.reset_index()))
-    benchmark_deck.append(
+
+    benchmark_raw_data = Deck("Benchmarking Raw Data")
+    benchmark_raw_data.append(TableRenderer().to_html(df=avg_tokens_per_second.reset_index()))
+    benchmark_raw_data.append(
         TableRenderer().to_html(df=step_peak_memory_reserved_mb.reset_index())
     )
 
-    benchmark_data_deck = Deck("Benchmarking Data")
-    benchmark_data_deck.append(FrameProfilingRenderer().to_html(df=analysis_df))
+    benchmark_data_profile = Deck("Benchmarking Data Profile")
+    benchmark_data_profile.append(
+        FrameProfilingRenderer().to_html(
+            df=analysis_df.assign(use_liger_int=lambda df: df["use_liger"].astype(int))
+        )
+    )
 
-    ctx.decks.insert(0, benchmark_data_deck)
+    ctx = current_context()
+    ctx.decks.insert(0, benchmark_raw_data)
+    ctx.decks.insert(0, benchmark_data_profile)
     ctx.decks.insert(0, benchmark_deck)
 
     return analysis_df
 
 
-@workflow
-def training_workflow(
-    experiment_args: ExperimentArguments = ExperimentArguments(),
-    training_args: TrainingArguments = TrainingArguments(),
-) -> TrainingResult:
-    return train_model(training_args=training_args)
+# ## Cache the parallelized experiment runs
+
+
+@dynamic(
+    container_image=image,
+    cache=True,
+    cache_version="1",
+)
+def run_cached_training_benchmark(
+    training_args_list: list[TrainingArguments],
+) -> list[Optional[TrainingResult]]:
+    results = map_task(
+        train_model,
+        min_success_ratio=0.1,
+        max_concurrency=4,
+    )(training_args=training_args_list)
+    return results
+
+
+# ## LLM benchmarking workflow
 
 
 @workflow
 def benchmarking_experiment(
     experiment_args: ExperimentArguments = ExperimentArguments(),
     training_args: TrainingArguments = TrainingArguments(),
+    n_runs: int = 3,
 ) -> tuple[list[Optional[TrainingResult]], pd.DataFrame]:
-    training_args_list = prepare_experiment_args(experiment_args, training_args)
-    results = map_task(
-        train_model,
-        min_success_ratio=0.1,
-        max_concurrency=4,
-    )(training_args=training_args_list)
+    training_args_list = prepare_experiment_args(experiment_args, training_args, n_runs)
+    results = run_cached_training_benchmark(training_args_list)
     analysis = analyze_results(results=results)
     return results, analysis
+
+
+# ## Results
+#
+# ![analysis results](static/analysis_results.png)
+
+# ## Appendix
 
 
 # Helper function to convert a matplotlib figure into an HTML string
