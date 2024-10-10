ae6b840bb6b9e4ae4461068ab17113306a15125d
Daniel Sola <daniel.sola@union.ai>
2024-10-09 17:48:40 -0700

    fix lp name
    Signed-off-by: Daniel Sola <daniel.sola@union.ai>

--- tutorials/vllm_serving_on_actor/lanchplans.py
+++ tutorials/vllm_serving_on_actor/lanchplans.py
@@ -2,10 +2,11 @@ from datetime import timedelta
 from flytekit import LaunchPlan, FixedRate
 from union.artifacts import OnArtifact
 
-from ner import TextSampleArtifact, ner_wf, upstream_wf
+from ner import TextSampleArtifact, ner_wf
+from upstream import upstream_wf
 
 ner_lp = LaunchPlan.get_or_create(
-    workflow=ner_wf, name="ner_lp_v4", trigger=OnArtifact(trigger_on=TextSampleArtifact)
+    workflow=ner_wf, name="ner_lp", trigger=OnArtifact(trigger_on=TextSampleArtifact)
 )
 
 upstream_lp = LaunchPlan.get_or_create(
