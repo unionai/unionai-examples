d129632ffb7c237b38f8506edb6e7b4f228b4383
Samhita Alla <aallasamhita@gmail.com>
2024-12-19 15:44:11 +0530

    add contextual rag example
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

---
+++ _blogs/together-contextual-rag/app.py
@@ -0,0 +1,78 @@
+import os
+
+import flytekit as fl
+from flytekit.core.artifact import Artifact
+from union.app import App, Endpoint, Input
+
+VectorDB = Artifact(name="vector-db-artifact")
+BM25Index = Artifact(name="bm25s-index")
+ContextualChunksJSON = Artifact(name="contextual-chunks-json")
+
+
+fastapi_app = Endpoint(
+    name="contextual-rag-fastapi-app",
+    inputs=[
+        Input(
+            name="vector_db",
+            value=VectorDB.query(),
+            auto_download=True,
+            env_name="VECTOR_DB",
+        ),
+        Input(
+            name="bm25s_index",
+            value=BM25Index.query(),
+            auto_download=True,
+            env_name="BM25S_INDEX",
+        ),
+        Input(
+            name="contextual_chunks_json",
+            value=ContextualChunksJSON.query(),
+            auto_download=True,
+            env_name="CONTEXTUAL_CHUNKS_JSON",
+        ),
+    ],
+    container_image=fl.ImageSpec(
+        name="contextual-rag-fastapi",
+        registry=os.getenv("REGISTRY"),
+        packages=[
+            "together",
+            "bm25s",
+            "chromadb",
+            "fastapi[standard]",
+            "union-runtime",
+        ],
+    ),
+    limits=fl.Resources(cpu="3", mem="10Gi"),
+    port=8080,
+    include=["./main.py"],
+    command=["fastapi", "dev", "--port", "8080"],
+    min_replicas=1,
+    max_replicas=1,
+)
+
+
+gradio_app = App(
+    name="contextual-rag-gradio-app",
+    inputs=[
+        Input(
+            name="fastapi_endpoint",
+            # value=fastapi_app.query_endpoint(public=False),
+            value="http://contextual-rag-fastapi-app.demo-development.svc.cluster.local",  # TODO: Remove when the fix is in.
+            env_name="FASTAPI_ENDPOINT",
+        )
+    ],
+    container_image=fl.ImageSpec(
+        name="contextual-rag-gradio",
+        registry=os.getenv("REGISTRY"),
+        packages=["gradio", "union-runtime"],
+    ),
+    limits=fl.Resources(cpu="1", mem="1Gi"),
+    port=8080,
+    include=["./gradio_app.py"],
+    command=[
+        "python",
+        "gradio_app.py",
+    ],
+    min_replicas=1,
+    max_replicas=1,
+)
