1217cf89bb1833ed0e2c98c4d3f174ea1b5bc3b0
Kevin Su <pingsutw@apache.org>
2024-12-20 12:58:35 -0800

    test
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- user_guide/core_concepts/artifacts/model_card.py
+++ user_guide/core_concepts/artifacts/model_card.py
@@ -5,7 +5,7 @@ from union.artifacts import ModelCard
 from typing_extensions import Annotated
 
 pandas_image = ImageSpec(
-    packages=["pandas==2.2.2", "pyarrow"]
+    packages=["pandas==2.2.2", "pyarrow", "tabulate"]
 )
 
 BasicArtifact = Artifact(name="my_basic_artifact")
