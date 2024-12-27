41023a6c3c74e1a336f6a582bb2d5ac56acc8088
Samhita Alla <aallasamhita@gmail.com>
2024-12-27 18:23:46 +0530

    update wikipedia embedding example
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- tutorials/wikipedia_embeddings_on_actor/wikipedia_embeddings_on_actor.py
+++ tutorials/wikipedia_embeddings_on_actor/wikipedia_embeddings_on_actor.py
@@ -1,33 +1,24 @@
 # # Generating Wikipedia Embeddings
 #
 # This tutorial demonstrates how to create embeddings for the Wikipedia dataset using Union actors.
-# Union actors enable efficient processing by initially loading the model onto the GPU and
-# keeping it ready for generating embeddings without reloading, making the process both fast and scalable.
+# Union actors facilitate efficient processing by keeping the model readily available for generating embeddings,
+# eliminating the need for repeated downloads.
 #
 # In short, by setting up an actor task, you can elevate a standard batch job to near-real-time inference.
 #
 # Let’s get started by importing the necessary libraries and modules:
 
+import functools
 import logging
 import pathlib
+import shutil
 from pathlib import Path
-from typing import Optional
 
+import flytekit as fl
 import pandas as pd
 import torch
-from flytekit import (
-    ImageSpec,
-    Resources,
-    Secret,
-    StructuredDataset,
-    current_context,
-    dynamic,
-    task,
-    workflow,
-)
 from flytekit.core.utils import timeit
 from flytekit.types.directory import FlyteDirectory
-from sentence_transformers import SentenceTransformer
 from union.actor import ActorEnvironment
 
 # ## Creating a secret
@@ -45,18 +36,17 @@ SERVERLESS_HF_KEY = "hf-api-key"
 
 # ## Defining the imagespec and actor environment
 #
-# The container image specification includes all required dependencies, 
+# The container image specification includes all required dependencies,
 # while the actor environment defines the runtime parameters for execution.
 
-embedding_image = ImageSpec(
+embedding_image = fl.ImageSpec(
     name="wikipedia_embedder",
     packages=[
         "datasets",
         "sentence_transformers",
         "pandas",
-        "flytekit>=1.13.9",
         "requests>=2.29.0",
-        "union>=0.1.86",
+        "union>=0.1.117",
     ],
     python_version="3.11",
 )
@@ -65,7 +55,7 @@ actor = ActorEnvironment(
     name="wikipedia-embedder-env",
     replica_count=20,
     ttl_seconds=900,
-    requests=Resources(gpu="1", mem="12Gi", cpu="5"),
+    requests=fl.Resources(gpu="1", mem="12Gi", cpu="5"),
     container_image=embedding_image,
 )
 
@@ -75,19 +65,20 @@ actor = ActorEnvironment(
 # ## Caching the model
 #
 # We cache the model download task to ensure the model remains available
-# between executions, avoiding redundant downloads.
+# between executions, avoiding redundant downloads from the HuggingFace hub.
 
-@task(
+
+@fl.task(
     cache=True,
     cache_version="0.1",
-    requests=Resources(mem="5Gi"),
-    secret_requests=[Secret(key=SERVERLESS_HF_KEY)],
+    requests=fl.Resources(mem="5Gi"),
+    secret_requests=[fl.Secret(key=SERVERLESS_HF_KEY)],
     container_image=embedding_image,
 )
 def download_model(embedding_model: str) -> FlyteDirectory:
     from huggingface_hub import login, snapshot_download
 
-    ctx = current_context()
+    ctx = fl.current_context()
     working_dir = Path(ctx.working_directory)
     cached_model_dir = working_dir / "cached_model"
 
@@ -95,48 +86,53 @@ def download_model(embedding_model: str) -> FlyteDirectory:
     snapshot_download(embedding_model, local_dir=cached_model_dir)
     return FlyteDirectory(path=cached_model_dir)
 
+
 # ## Defining an actor task
 #
-# We define an actor task to handle data encoding.
-# The encoder is set as a global variable to avoid re-initializing it with each task execution.
-
-encoder: Optional[SentenceTransformer] = None
-
-
-def load_model(local_model_path: str) -> SentenceTransformer:
-    global encoder
-    if encoder:
-        return encoder
-    encoder = SentenceTransformer(local_model_path)
-    encoder.max_seq_length = 256
-    return encoder
+# An actor task is used to handle data encoding efficiently.
+# To optimize performance, the model is not reloaded if it is already available locally.
+# Actor tasks enable this by allowing you to reuse a container across multiple tasks.
 
 
 @actor.task(cache=True, cache_version="1.1")
 def encode(
     df: pd.DataFrame, batch_size: int, model_dir: FlyteDirectory
 ) -> torch.Tensor:
-    bi_encoder = load_model(model_dir.download())
-    print(f"Loaded encoder into device: {bi_encoder.device}")
-    return bi_encoder.encode(
+    from sentence_transformers import SentenceTransformer
+
+    local_path = Path("/tmp/embedding-model")
+
+    if not local_path.exists():
+        temp_local_path = model_dir.download()
+        shutil.copytree(src=temp_local_path, dst=str(local_path))
+
+    encoder = SentenceTransformer(str(local_path))
+    encoder.max_seq_length = 256
+
+    print(f"Loaded encoder into device: {encoder.device}")
+    return encoder.encode(
         df["text"],
         convert_to_tensor=True,
         show_progress_bar=True,
         batch_size=batch_size,
     )
 
+
 # ## Defining a partitions task
 #
 # Next, we define a task to list all partitions for the dataset.
 # This task downloads the data and iterates over the directory to create partitions.
 
-@task(
+
+@fl.task(
     container_image=embedding_image,
-    requests=Resources(mem="4Gi", cpu="3"),
+    requests=fl.Resources(mem="4Gi", cpu="3"),
     cache=True,
     cache_version="1.1",
 )
-def list_partitions(name: str, version: str, num_proc: int) -> list[StructuredDataset]:
+def list_partitions(
+    name: str, version: str, num_proc: int
+) -> list[fl.StructuredDataset]:
     from datasets import DownloadConfig, load_dataset_builder
 
     dsb = load_dataset_builder(
@@ -155,32 +151,20 @@ def list_partitions(name: str, version: str, num_proc: int) -> list[StructuredDa
     for f in p.iterdir():
         if "parquet" in f.name:
             print(f"Encoding {i}: {f}")
-            partitions.append(StructuredDataset(uri=str(f)))
+            partitions.append(fl.StructuredDataset(uri=str(f)))
         i += 1
     return partitions
 
-# ## Defining workflows
+
+# ## Defining a workflow
 #
-# We define a dynamic workflow that iterates through the partitions and invokes the encode task to generate embeddings. 
+# We define a workflow that sequentially calls all tasks, including the map task,
+# which iterates through the partitions and invokes the encode task to generate embeddings.
 # After the initial run, subsequent encode tasks reuse the actor environment, resulting in faster encoding.
-
-@dynamic(
-    container_image=embedding_image,
-    cache=True,
-    cache_version="1.1",
-)
-def dynamic_encoder(
-    partitions: list[StructuredDataset], model_dir: FlyteDirectory, batch_size: int
-) -> list[torch.Tensor]:
-    embeddings = []
-    for p in partitions:
-        embeddings.append(encode(df=p, batch_size=batch_size, model_dir=model_dir))
-    return embeddings
-
-# Lastly, we define a workflow that sequentially calls all tasks, including the dynamic workflow. 
 # This workflow returns a list of embeddings as Torch tensors.
 
-@workflow
+
+@fl.workflow
 def embed_wikipedia(
     name: str = "wikipedia",
     version: str = "20220301.en",
@@ -190,6 +174,6 @@ def embed_wikipedia(
 ) -> list[torch.Tensor]:
     partitions = list_partitions(name=name, version=version, num_proc=num_proc)
     model_dir = download_model(embedding_model=embedding_model)
-    return dynamic_encoder(
-        partitions=partitions, model_dir=model_dir, batch_size=batch_size
-    )
+    return fl.map_task(
+        functools.partial(encode, batch_size=batch_size, model_dir=model_dir)
+    )(df=partitions)
