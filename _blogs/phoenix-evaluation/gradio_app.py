102868fbc21df22316521f2b481fc85b09c825b1
Samhita Alla <aallasamhita@gmail.com>
2025-02-06 22:25:13 -0800

    update app
    Signed-off-by: Samhita Alla <aallasamhita@gmail.com>

--- _blogs/phoenix-evaluation/gradio_app.py
+++ _blogs/phoenix-evaluation/gradio_app.py
@@ -5,26 +5,25 @@ from openai import OpenAI
 from openinference.instrumentation.openai import OpenAIInstrumentor
 from phoenix.otel import register
 
-os.environ["OTEL_EXPORTER_OTLP_HEADERS"] = f"api_key={os.environ('PHOENIX_API_KEY')}"
-os.environ["PHOENIX_CLIENT_HEADERS"] = f"api_key={os.environ('PHOENIX_API_KEY')}"
+os.environ["OTEL_EXPORTER_OTLP_HEADERS"] = f"api_key={os.getenv('PHOENIX_API_KEY')}"
+os.environ["PHOENIX_CLIENT_HEADERS"] = f"api_key={os.getenv('PHOENIX_API_KEY')}"
 os.environ["PHOENIX_COLLECTOR_ENDPOINT"] = "https://app.phoenix.arize.com"
 
-tracer_provider = register() 
+tracer_provider = register()
 
 OpenAIInstrumentor().instrument(tracer_provider=tracer_provider)
+
 
 def stream_response(query, history):
     history.append({"role": "user", "content": query})
 
     client = OpenAI(
-        base_url=f"{os.environ('VLLM_DEEPSEEK_ENDPOINT')}/v1",
+        base_url=f"{os.getenv('VLLM_DEEPSEEK_ENDPOINT')}/v1",
         api_key="random",
     )
 
     response = client.chat.completions.create(
-        model="NousResearch/Meta-Llama-3-8B-Instruct",
-        messages=history,
-        stream=True
+        model="NousResearch/Meta-Llama-3-8B-Instruct", messages=history, stream=True
     )
 
     for chunk in response:
@@ -34,9 +33,7 @@ def stream_response(query, history):
 
 
 demo = gr.ChatInterface(
-    stream_response,
-    type="messages",
-    title="DeepSeek R1 Chatbot"
+    stream_response, type="messages", title="DeepSeek R1 Distill Qwen 1.5B Chatbot"
 )
 
 demo.launch(server_port=8080)
