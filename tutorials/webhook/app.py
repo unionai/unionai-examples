1f850fa32fca38bacc98172cf4755a3442c7d643
Thomas J. Fan <thomasjpfan@gmail.com>
2025-03-04 16:55:08 -0500

    Pin fastapi

--- tutorials/webhook/app.py
+++ tutorials/webhook/app.py
@@ -12,7 +12,7 @@ from union.app import App
 
 image_spec = ImageSpec(
     name="webhook-serving",
-    packages=["union-runtime>=0.1.11", "fastapi[standard]", "union>=0.1.150"],
+    packages=["union-runtime>=0.1.11", "fastapi[standard]==0.115.11", "union>=0.1.150"],
     registry=os.getenv("IMAGE_SPEC_REGISTRY"),
 )
 
