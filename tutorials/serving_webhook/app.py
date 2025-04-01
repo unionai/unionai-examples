566a1d8386a3fc7e01c12392af51def315dac05f
Niels Bantilan <niels.bantilan@gmail.com>
2025-04-01 13:44:22 -0400

    Update webhook example with {{run-on-union}}

--- tutorials/serving_webhook/app.py
+++ tutorials/serving_webhook/app.py
@@ -4,6 +4,8 @@
 # any of your registered workflows. We will learn how to use a simple `cURL` command with
 # authentication to easily start your workflows!
 
+# {{run-on-union}}
+
 # ## Defining the Application Configuration
 #
 # First, we define the image spec for the runtime image. We will use `fastapi` for defining
