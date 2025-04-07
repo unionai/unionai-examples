ad22fd8e1c671ae78aea3909d36c36508eea327c
Niels Bantilan <niels.bantilan@gmail.com>
2025-04-07 16:33:32 -0400

    reduce memory footprint
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- tutorials/vector_store_lance_db/vector_store_lance_db.py
+++ tutorials/vector_store_lance_db/vector_store_lance_db.py
@@ -22,6 +22,7 @@ image = union.ImageSpec(
         "pyarrow",
         "pymupdf",
         "sentence-transformers",
+        "tqdm",
     ]
 )
 
@@ -51,6 +52,7 @@ class TestQuery:
 
 @union.task(container_image=image, cache=True, cache_version="0")
 def download_arxiv_papers(query: str, max_results: int) -> union.FlyteDirectory:
+    """Download the papers from Arxiv given a query."""
     import arxiv
 
     client = arxiv.Client()
@@ -74,6 +76,7 @@ def download_arxiv_papers(query: str, max_results: int) -> union.FlyteDirectory:
 
 @union.task(container_image=image, cache=True, cache_version="0")
 def extract_documents(arxiv_dir: union.FlyteDirectory) -> union.FlyteDirectory:
+    """Extract raw text from the PDF files."""
     import os
     import pymupdf
 
@@ -92,12 +95,18 @@ def extract_documents(arxiv_dir: union.FlyteDirectory) -> union.FlyteDirectory:
 
 
 def chunk_document(document: str, config: VectorStoreConfig) -> list[str]:
+    """Helper function to chunk a document into smaller chunks."""
 
+    # Try to split the documents by iterating through the provided chunk
+    # delimiters. If the largest chunk is smaller than the approximate_chunk_size,
+    # we select that delimiter.
     for delimiter in config.chunk_delimiters:
         _chunks = document.split(delimiter)
         if max(len(x) for x in _chunks) < config.approximate_chunk_size:
             break
 
+    # Consolidate the chunks such that chunks are about the size specified by
+    # the approximate_chunk_size.
     chunks: list[str] = []
     _new_chunk = ""
     for chunk in _chunks:
@@ -122,54 +131,59 @@ def create_vector_store(
     documents_dir: union.FlyteDirectory,
     config: VectorStoreConfig,
 ) -> tuple[Annotated[union.FlyteDirectory, VectorStore], TestQuery]:
+    """Create a vector store from a directory of documents."""
     import lancedb
     import pyarrow as pa
+    import tqdm
+
     from sentence_transformers import SentenceTransformer
 
-    documents_dir.download()
-    documents_dir: Path = Path(documents_dir)
-
-    document_chunks: list[list[str]] = []
-    print("chunking documents")
-
-    paper_ids = []
-    for document_fp in documents_dir.glob("**/*.txt"):
-        with open(document_fp, "rb") as f:
-            document = f.read().decode("utf-8")
-            chunks = chunk_document(document, config)
-            document_chunks.append(chunks)
-            paper_ids.append(document_fp.stem)
-
     model = SentenceTransformer(EMBEDDING_MODEL)
-    document_vectors = []
-    vectors = model.encode(document_chunks)
-    print("embedding documents")
-    for chunks in document_chunks:
-        document_vectors.append(model.encode(chunks))
 
-    data = []
-    for paper_id, vectors, chunks in zip(paper_ids, document_vectors, document_chunks):
-        for vector, chunk in zip(vectors, chunks):
-            data.append(
-                {
-                    "paper_id": paper_id,
-                    "vector": vector,
-                    "text": chunk,
-                }
-            )
-
+    # Create a LanceDB database and table.
     lancedb_dir = f"{union.current_context().working_directory}/lancedb"
     db = lancedb.connect(lancedb_dir)
     schema = pa.schema(
         {
             "paper_id": pa.string(),
-            "vector": pa.list_(pa.float32(), len(document_vectors[0][0])),
+            "vector": pa.list_(pa.float32(), model.get_sentence_embedding_dimension()),
             "text": pa.string(),
         }
     )
-    table = db.create_table("papers", data, schema=schema)
+    table = db.create_table("papers", schema=schema)
+
+    documents_dir.download()
+    documents_dir: Path = Path(documents_dir)
+
+    document_paths = list(documents_dir.glob("**/*.txt"))
+
+    # Iterate through the documents and add them to the vector store.
+    for i, document_fp in tqdm.tqdm(
+        enumerate(document_paths),
+        total=len(document_paths),
+        desc="chunking and embedding documents",
+    ):
+
+        with open(document_fp, "rb") as f:
+            document = f.read().decode("utf-8")
+
+        chunks = chunk_document(document, config)
+        vectors = model.encode(chunks)
+        data = [
+            {
+                "paper_id": document_fp.stem,
+                "vector": vector,
+                "text": chunk,
+            }
+            for vector, chunk in zip(vectors, chunks)
+        ]
+        table.add(data)
+        if i == 0:
+            test_query = TestQuery(data[0]["text"], data[0]["paper_id"])
+
+    # table.create_index("vector", index_type="hnsw")
     table.create_scalar_index("paper_id", index_type="BITMAP")
-    return union.FlyteDirectory(lancedb_dir), TestQuery(document_chunks[0][0], paper_ids[0])
+    return union.FlyteDirectory(lancedb_dir), test_query
 
 
 @union.task(container_image=image)
@@ -177,6 +191,11 @@ def validate_vector_store(
     vector_store: union.FlyteDirectory,
     test_query: TestQuery,
 ):
+    """Validate the vector store by querying it with a test query.
+
+    Test query should be a document from the vector store, which guarantees
+    that the vector store returns a result with close-to-zero distance.
+    """
     import lancedb
     from numpy.testing import assert_allclose
     from sentence_transformers import SentenceTransformer
@@ -187,7 +206,7 @@ def validate_vector_store(
     table = db.open_table("papers")
     vector_query = model.encode(test_query.query)
     result = (
-        table.search(vector_query)
+        table.search(vector_query, "vector")
         .limit(1)
         .where(f"paper_id = '{test_query.paper_id}'")
         .to_list()
@@ -206,6 +225,7 @@ def main(
     max_results: int,
     config: VectorStoreConfig = VectorStoreConfig(),
 ) -> union.FlyteDirectory:
+    """Main workflow to create a vector store from Arxiv papers."""
     arxiv_papers = download_arxiv_papers(query, max_results)
     parsed_papers = extract_documents(arxiv_papers)
     vector_store, test_query = create_vector_store(parsed_papers, config)
