3b762d099a856bf673e2a7bf661332fe63b0a35a
Peeter Piegaze <1153481+ppiegaze@users.noreply.github.com>
2025-09-18 14:09:11 +0200

    update

--- user-guide-v2/task-configuration/multiple-environments/af2/fold/run.py
+++ user-guide-v2/task-configuration/multiple-environments/af2/fold/run.py
@@ -5,10 +5,10 @@ FOLD_PACKAGES = ["ruff"]
 
 fold_image = flyte.Image.from_debian_base().with_pip_packages(*FOLD_PACKAGES)
 
-env = flyte.TaskEnvironment(name="multi_fold", image=fold_image)
+fold_env = flyte.TaskEnvironment(name="fold_env", image=fold_image)
 
 
-@env.task
+@fold_env.task
 def run_fold(sequence: str, msa: File) -> list[str]:
     with msa.open_sync("r") as f:
         msa_content = f.read()
