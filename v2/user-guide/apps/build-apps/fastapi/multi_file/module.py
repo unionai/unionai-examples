163392b4d7a0758e519c5025b0c7ad346fb13d9f
Niels Bantilan <niels.bantilan@gmail.com>
2025-12-12 19:51:07 -0500

    delete
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- v2/user-guide/apps/build-apps/fastapi/multi_file/module.py
+++
@@ -1,6 +0,0 @@
-# {{docs-fragment helper-function}}
-def function():
-    """Helper function used by the FastAPI app."""
-    return {"message": "Hello from module.py!"}
-# {{/docs-fragment helper-function}}
-
