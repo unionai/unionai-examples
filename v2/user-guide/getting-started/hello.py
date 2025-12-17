ae7421a636fce6c723a0968d50a000865ed90bf4
Peeter Piegaze <1153481+ppiegaze@users.noreply.github.com>
2025-12-17 14:43:20 +0100

    Simplify example

--- v2/user-guide/getting-started/hello.py
+++ v2/user-guide/getting-started/hello.py
@@ -1,54 +1,23 @@
-# /// script
-# requires-python = "==3.13"
-# dependencies = [
-#    "flyte==2.0.0b31",
-# ]
-# main = "main"
-# params = "x_list=[1,2,3,4,5,6,7,8,9,10]"
-# ///
+# hello.py
 
 import flyte
 
-# A TaskEnvironment provides a way of grouping the configuration used by tasks.
-env = flyte.TaskEnvironment(name="hello_world")
+# The `hello_env` TaskEnvironment is assgned to the variable `env`.
+# It is then used in the `@env.task` decorator to define tasks.
+# The environment groups configuration for all tasks defined within it.
+env = flyte.TaskEnvironment(name="hello_env")
 
-
-# Use a TaskEnvironment to define tasks, which are regular Python functions.
+# We use the `@env.task` decorator to define a task called `fn`.
 @env.task
-def fn(x: int) -> int: # Type annotations are recommended.
+def fn(x: int) -> int: # Type annotations are required
     slope, intercept = 2, 5
-    #raise ValueError("I will fail!")
     return slope * x + intercept
 
-
-# Tasks can call other tasks.
-# Each task defined with a given TaskEnvironment will run in its own separate container,
-# but the containers will all be configured identically.
+# We also use the `@env.task` decorator to define another task called `main`.
+# This is the is the entrypoint task of the workflow.
+# It calls the `fn` task defined above multiple times using `flyte.map`.
 @env.task
 def main(x_list: list[int] = list(range(10))) -> float:
-    x_len = len(x_list)
-    if x_len < 10:
-        raise ValueError(f"x_list doesn't have a larger enough sample size, found: {x_len}")
-
-    # flyte.map is like Python map, but runs in parallel.
-    y_list = list(flyte.map(fn, x_list))
+    y_list = list(flyte.map(fn, x_list)) # flyte.map is like Python map, but runs in parallel.
     y_mean = sum(y_list) / len(y_list)
     return y_mean
-
-
-# Running this script locally will perform a flyte.run,
-# which will deploy your task code to your remote Union/Flyte instance.
-if __name__ == "__main__":
-
-    # Initialize Flyte from a config file.
-    flyte.init_from_config()
-
-    # Run your tasks remotely inline and pass parameter data.
-    r = flyte.run(main, x_list=list(range(10)))
-
-    # Print various attributes of the run.
-    print(r.name)
-    print(r.url)
-
-    # Stream the logs from the remote run to the terminal.
-    r.wait()
