f60ab3b4689856732c757550cd054c98dc0f3c26
Niels Bantilan <niels.bantilan@gmail.com>
2026-01-09 16:07:22 -0500

    update basic streamlit app
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- v2/user-guide/build-apps/streamlit/basic_streamlit.py
+++ v2/user-guide/build-apps/streamlit/basic_streamlit.py
@@ -7,14 +7,12 @@
 
 """A basic Streamlit app using the built-in hello demo."""
 
+# {{docs-fragment app-definition}}
 import flyte
 import flyte.app
 
-# {{docs-fragment image}}
 image = flyte.Image.from_debian_base(python_version=(3, 12)).with_pip_packages("streamlit==1.41.1")
-# {{/docs-fragment image}}
 
-# {{docs-fragment app-env}}
 app_env = flyte.app.AppEnvironment(
     name="streamlit-hello",
     image=image,
@@ -23,12 +21,9 @@ app_env = flyte.app.AppEnvironment(
     resources=flyte.Resources(cpu="1", memory="1Gi"),
     requires_auth=False,
 )
-# {{/docs-fragment app-env}}
 
-# {{docs-fragment deploy}}
 if __name__ == "__main__":
     flyte.init_from_config()
     app = flyte.deploy(app_env)
     print(f"Deployed app: {app[0].summary_repr()}")
-# {{/docs-fragment deploy}}
-
+# {{/docs-fragment app-definition}}
