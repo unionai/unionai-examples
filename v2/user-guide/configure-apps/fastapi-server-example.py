74f62830aa36aabd44bde1f2d3f3866a8f35720b
Niels Bantilan <niels.bantilan@gmail.com>
2026-01-09 15:38:16 -0500

    update
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

--- v2/user-guide/configure-apps/fastapi-server-example.py
+++ v2/user-guide/configure-apps/fastapi-server-example.py
@@ -36,6 +36,23 @@ async def root() -> dict:
     return {"message": "Hello from FastAPI!"}
 # {{/docs-fragment fastapi-app}}
 
+
+# {{docs-fragment on-startup-decorator}}
+state = {}
+
+@env.on_startup
+async def app_startup():
+    print("App started up")
+    state["data"] = ["Here's", "some", "data"]
+# {{/docs-fragment on-startup-decorator}}
+
+# {{docs-fragment on-shutdown-decorator}}
+@env.on_shutdown
+async def app_shutdown():
+    print("App shut down")
+    state.clear()  # clears the data
+# {{/docs-fragment on-shutdown-decorator}}
+
 # {{docs-fragment deploy}}
 if __name__ == "__main__":
     import logging
