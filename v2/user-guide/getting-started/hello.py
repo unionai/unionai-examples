2b2e64bd970510057162b1cdbb26637cbe4495ab
Niels Bantilan <niels.bantilan@gmail.com>
2026-08-26 10:17:39 -0400

    add back hello.py
    Signed-off-by: Niels Bantilan <niels.bantilan@gmail.com>

---
+++ v2/user-guide/getting-started/hello.py
@@ -0,0 +1,23 @@
+# hello.py
+
+import flyte
+
+# The `hello_env` TaskEnvironment is assigned to the variable `env`.
+# It is then used in the `@env.task` decorator to define tasks.
+# The environment groups configuration for all tasks defined within it.
+env = flyte.TaskEnvironment(name="hello_env")
+
+# We use the `@env.task` decorator to define a task called `fn`.
+@env.task
+def fn(x: int) -> int: # Type annotations are required
+    slope, intercept = 2, 5
+    return slope * x + intercept
+
+# We also use the `@env.task` decorator to define another task called `main`.
+# This is the entrypoint task of the workflow.
+# It calls the `fn` task defined above multiple times using `flyte.map`.
+@env.task
+def main(x_list: list[int] = list(range(10))) -> float:
+    y_list = list(flyte.map(fn, x_list)) # flyte.map is like Python map, but runs in parallel.
+    y_mean = sum(y_list) / len(y_list)
+    return y_mean
